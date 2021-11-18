7f1bdbe98e2177c9356343670ab01eae8eba8d45
Werner Dia Black <werner.diablack@outlook.com>
2021-11-18 15:13:32 +0100

    Backend

---
+++ Backend/index.js
@@ -0,0 +1,28 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+  }
+});
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+io.on('connection' , (socket) => {
+  console.log('user connected'+ socket.id);
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  
+  //socket.broadcast.emit("hello", "world");
+  socket.emit("init", {message: "elemaan"});
+
+
+})
+
+
+
+http.listen(4000, () => {
+  console.log('listening on Port:4000');
+});
